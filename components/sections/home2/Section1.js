4c2a75872b818e34d914374b406a8e3c672e5569
Diego <dmercadoff@gmail.com>
2024-06-15 02:16:38 -0400

    oa

--- components/sections/home2/Section1.js
+++ components/sections/home2/Section1.js
@@ -1,13 +1,13 @@
 
 
-// import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline';
 
 export default function Section1() {
     return (
         <>
             <section className="section-box">
                 <div className='container-spline'>
-                    {/* <Spline scene="https://prod.spline.design/ZgdSd1EG3vLDL3gn/scene.splinecode" /> */}
+                    <Spline scene="https://prod.spline.design/ZgdSd1EG3vLDL3gn/scene.splinecode" />
                 </div>
             </section>
         </>
