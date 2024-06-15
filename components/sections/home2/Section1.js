02bc27523b86fd351600b5cbaeafd3d76f1995c6
Diego <dmercadoff@gmail.com>
2024-06-15 02:11:28 -0400

    oa

---
+++ components/sections/home2/Section1.js
@@ -0,0 +1,15 @@
+
+
+// import Spline from '@splinetool/react-spline';
+
+export default function Section1() {
+    return (
+        <>
+            <section className="section-box">
+                <div className='container-spline'>
+                    {/* <Spline scene="https://prod.spline.design/ZgdSd1EG3vLDL3gn/scene.splinecode" /> */}
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+// next.config.js
+module.exports = {
+
+    output: 'export',
+  };
+  
+
+module.exports = nextConfig
+
+
